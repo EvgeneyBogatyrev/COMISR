7a4b5fc214cde92d1970105bd237328ba5449651
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-08-22 16:06:12 +0300

    tmp

--- run.py
+++ run.py
@@ -8,9 +8,9 @@ import subprocess
 #os.system("chmod 0777 /model/run.sh")
 #os.system("/model/run.sh")
 
-videos = os.listdir("/dataset")
-for video in videos:
-    subprocess.run(["mkdir", f"/results/{video}"])
-    subprocess.run(["python3", "/model/inference_and_eval.py", "--checkpoint_path=/tmp/model.ckpt", 
-        f"--input_lr_dir=/dataset/{video}", f"--output_dir=/result/{video}"])
+#videos = os.listdir("/dataset")
+#for video in videos:
+  #  subprocess.run(["mkdir", f"/results/{video}"])
+subprocess.run(["python3", "/model/inference_and_eval.py", "--checkpoint_path=/tmp/model.ckpt", 
+        f"--input_lr_dir=/dataset", f"--output_dir=/result"])
 #   subprocess.run(["python3", "/model/.py", "-n", "RealESRGAN_x4plus", "--input", f"/dataset/{video}", "--face_enhance", "--tile", "1024", "--output", f"/results/{video}"])
