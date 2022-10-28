d6e99e968d38542a7e5be1f6fa948e958d61a302
Evgeney Zimin <evgeney.zimin@graphics.cs.msu.ru>
2022-10-28 10:54:17 +0300

    Fix (VG)run.py

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ os.system("chmod -R 0777 /results")
 videos = os.listdir("/dataset")
 for video in videos:
     subprocess.run(["mkdir", f"/results/{video}"])
-    subprocess.run(["python3", "/model/inference_and_eval.py", "--checkpoint_path=/tmp/model.ckpt", 
+subprocess.run(["python3", "/model/inference_and_eval.py", "--checkpoint_path=/tmp/model.ckpt", 
         f"--input_lr_dir=/dataset", f"--output_dir=/results"])
 #   subprocess.run(["python3", "/model/.py", "-n", "RealESRGAN_x4plus", "--input", f"/dataset/{video}", "--face_enhance", "--tile", "1024", "--output", f"/results/{video}"])
